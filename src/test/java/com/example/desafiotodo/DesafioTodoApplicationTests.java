41c310ee66b0db7d098ff283a4b2332fa55df1ad
pedrogml <pedrogabriel3@gmail.com>
2025-01-21 01:32:18 -0300

    Add files

---
+++ src/test/java/com/example/desafiotodo/DesafioTodoApplicationTests.java
@@ -0,0 +1,48 @@
+package com.example.desafiotodo;
+
+import com.example.desafiotodo.entity.Todo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DesafioTodoApplicationTests {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void testCreateTodoSucesso() {
+
+        var todo = new Todo("todo 1", "desc todo 1", false, 1 );
+
+        webTestClient
+                .post()
+                .uri("/todos")
+                .bodyValue(todo)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$").isArray()
+                .jsonPath("$.length()").isEqualTo(1)
+                .jsonPath("$[0].nome").isEqualTo(todo.getNome())
+                .jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+                .jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+                .jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
+
+    }
+
+    @Test
+    void testListTodoFailure() {
+
+        webTestClient
+                .post()
+                .uri("/todos")
+                .bodyValue(
+                        new Todo("", "", false, 0 )
+                ).exchange()
+                .expectStatus().isBadRequest();
+    }
+
+}
